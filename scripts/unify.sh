63c712a52ab6e427af1ea034fe84d44c21ab9a3a
Giorgos Kourtellos <giorgos@pumapay.io>
2018-04-30 11:41:47 +0300

    unify only the token

--- scripts/unify.sh
+++ scripts/unify.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-./node_modules/.bin/truffle-flattener ../contracts/RefundVault.sol >> ./unified.sol
+./node_modules/.bin/truffle-flattener ../contracts/PumaPayToken.sol >> ./unified.sol
