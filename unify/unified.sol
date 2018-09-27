7998098f29613ba16172518de5d0b676ddba3762
Giorgos Kourtellos <giorgos@pumapay.io>
2018-09-27 11:26:08 +0300

    unifying

--- unify/unified.sol
+++ unify/unified.sol
@@ -418,13 +418,13 @@ contract PumaPayToken is MintableToken {
 /// @title PumaPay Pull Payment - Contract that facilitates our pull payment protocol
 /// @author PumaPay Dev Team - <developers@pumapay.io>
 contract PumaPayPullPayment is Ownable {
-    
+
     using SafeMath for uint256;
 
     /// ===============================================================================================================
     ///                                      Events
     /// ===============================================================================================================
-    
+
     event LogExecutorAdded(address executor);
     event LogExecutorRemoved(address executor);
     event LogPaymentRegistered(address clientAddress, address beneficiaryAddress, string paymentID);
@@ -439,17 +439,18 @@ contract PumaPayPullPayment is Ownable {
     uint256 constant private DECIMAL_FIXER = 10000000000; /// 1e^10 - This transforms the Rate from decimals to uint256
     uint256 constant private FIAT_TO_CENT_FIXER = 100;    /// Fiat currencies have 100 cents in 1 basic monetary unit.
     uint256 constant private ONE_ETHER = 1 ether;         /// PumaPay token has 18 decimals - same as one ETHER
-    uint256 constant private MINIMUM_AMOUN_OF_ETH_FOR_OPARATORS = 0.01 ether; /// minimum amount of ETHER the owner/executor should have 
-    
+    uint256 constant private MINIMUM_AMOUNT_OF_ETH_FOR_OPARATORS = 0.01 ether; /// minimum amount of ETHER the owner/executor should have
+    uint256 constant private OVERFLOW_LIMITER_NUMBER = 10 ** 20; /// 1e^20 - This number is used to prevent numeric overflows
+
     /// ===============================================================================================================
     ///                                      Members
     /// ===============================================================================================================
 
     PumaPayToken public token;
 
-    mapping (string => uint256) private exchangeRates;
-    mapping (address => bool) public executors;
-    mapping (address => mapping (address => PullPayment)) public pullPayments;
+    mapping(string => uint256) private exchangeRates;
+    mapping(address => bool) public executors;
+    mapping(address => mapping(address => PullPayment)) public pullPayments;
 
     struct PullPayment {
         string merchantID;                      /// ID of the merchant
@@ -468,21 +469,20 @@ contract PumaPayPullPayment is Ownable {
     /// ===============================================================================================================
     ///                                      Modifiers
     /// ===============================================================================================================
-
     modifier isExecutor() {
-         require(executors[msg.sender]);
-         _;
-     }
+        require(executors[msg.sender]);
+        _;
+    }
 
-     modifier executorExists(address _executor) {
-         require(executors[_executor]);
-         _;
-     }
+    modifier executorExists(address _executor) {
+        require(executors[_executor]);
+        _;
+    }
 
-     modifier executorDoesNotExists(address _executor) {
-         require(!executors[_executor]);
-         _;
-     } 
+    modifier executorDoesNotExists(address _executor) {
+        require(!executors[_executor]);
+        _;
+    }
 
     modifier paymentExists(address _client, address _beneficiary) {
         require(doesPaymentExist(_client, _beneficiary));
@@ -499,14 +499,14 @@ contract PumaPayPullPayment is Ownable {
             (pullPayments[_client][_beneficiary].initialPaymentAmountInCents > 0 ||
             (now >= pullPayments[_client][_beneficiary].startTimestamp &&
             now >= pullPayments[_client][_beneficiary].nextPaymentTimestamp)
-            ) 
+            )
             &&
             pullPayments[_client][_beneficiary].numberOfPayments > 0 &&
-            (pullPayments[_client][_beneficiary].cancelTimestamp == 0 ||
-            pullPayments[_client][_beneficiary].cancelTimestamp > pullPayments[_client][_beneficiary].nextPaymentTimestamp) &&
-            keccak256(
-                abi.encodePacked(pullPayments[_client][_beneficiary].paymentID)
-                ) == keccak256(abi.encodePacked(_paymentID))
+        (pullPayments[_client][_beneficiary].cancelTimestamp == 0 ||
+        pullPayments[_client][_beneficiary].cancelTimestamp > pullPayments[_client][_beneficiary].nextPaymentTimestamp) &&
+        keccak256(
+            abi.encodePacked(pullPayments[_client][_beneficiary].paymentID)
+        ) == keccak256(abi.encodePacked(_paymentID))
         );
         _;
     }
@@ -539,19 +539,19 @@ contract PumaPayPullPayment is Ownable {
     }
 
     // @notice Will receive any eth sent to the contract
-    function () external payable {
+    function() external payable {
     }
 
     /// ===============================================================================================================
     ///                                      Public Functions - Owner Only
     /// ===============================================================================================================
-    
+
     /// @dev Adds a new executor. - can be executed only by the onwer. 
     /// When adding a new executor 1 ETH is tranferred to allow the executor to pay for gas.
     /// The balance of the owner is also checked and if funding is needed 1 ETH is transferred.
     /// @param _executor - address of the executor which cannot be zero address.
     function addExecutor(address _executor)
-    public 
+    public
     onlyOwner
     isValidAddress(_executor)
     executorDoesNotExists(_executor)
@@ -562,7 +562,7 @@ contract PumaPayPullPayment is Ownable {
         if (isFundingNeeded(owner)) {
             owner.transfer(1 ether);
         }
-        
+
         emit LogExecutorAdded(_executor);
     }
 
@@ -570,7 +570,7 @@ contract PumaPayPullPayment is Ownable {
     /// The balance of the owner is checked and if funding is needed 1 ETH is transferred.
     /// @param _executor - address of the executor which cannot be zero address.
     function removeExecutor(address _executor)
-    public 
+    public
     onlyOwner
     isValidAddress(_executor)
     executorExists(_executor)
@@ -593,7 +593,7 @@ contract PumaPayPullPayment is Ownable {
     returns (bool) {
         exchangeRates[_currency] = _rate;
         emit LogSetExchangeRate(_currency, _rate);
-        
+
         if (isFundingNeeded(owner)) {
             owner.transfer(1 ether);
         }
@@ -621,7 +621,7 @@ contract PumaPayPullPayment is Ownable {
     /// @param _frequency - how often merchant can pull - in seconds.
     /// @param _numberOfPayments - amount of pull payments merchant can make
     /// @param _startTimestamp - when subscription starts - in seconds.
-    function registerPullPayment (
+    function registerPullPayment(
         uint8 v,
         bytes32 r,
         bytes32 s,
@@ -644,11 +644,12 @@ contract PumaPayPullPayment is Ownable {
             bytes(_currency).length > 0 &&
             _client != address(0) &&
             _beneficiary != address(0) &&
-            _initialPaymentAmountInCents >= 0 &&
             _fiatAmountInCents > 0 &&
             _frequency > 0 &&
+            _frequency < OVERFLOW_LIMITER_NUMBER &&
             _numberOfPayments > 0 &&
-            _startTimestamp > 0
+            _startTimestamp > 0 &&
+            _startTimestamp < OVERFLOW_LIMITER_NUMBER
         );
 
         pullPayments[_client][_beneficiary].currency = _currency;
@@ -665,7 +666,7 @@ contract PumaPayPullPayment is Ownable {
         pullPayments[_client][_beneficiary].nextPaymentTimestamp = _startTimestamp;
         pullPayments[_client][_beneficiary].lastPaymentTimestamp = 0;
         pullPayments[_client][_beneficiary].cancelTimestamp = 0;
-        
+
         if (isFundingNeeded(msg.sender)) {
             msg.sender.transfer(1 ether);
         }
@@ -684,7 +685,7 @@ contract PumaPayPullPayment is Ownable {
     /// @param _paymentID - ID of the payment.
     /// @param _client - client address that is linked to this pull payment.
     /// @param _beneficiary - address that is allowed to execute this pull payment.
-    function deletePullPayment (
+    function deletePullPayment(
         uint8 v,
         bytes32 r,
         bytes32 s,
@@ -697,7 +698,7 @@ contract PumaPayPullPayment is Ownable {
     paymentExists(_client, _beneficiary)
     paymentNotCancelled(_client, _beneficiary)
     isValidDeletionRequest(_paymentID, _client, _beneficiary)
-    {   
+    {
         require(isValidDeletion(v, r, s, _paymentID, _client, _beneficiary));
 
         pullPayments[_client][_beneficiary].cancelTimestamp = now;
@@ -738,8 +739,8 @@ contract PumaPayPullPayment is Ownable {
             amountInPMA = calculatePMAFromFiat(pullPayments[_client][msg.sender].initialPaymentAmountInCents, pullPayments[_client][msg.sender].currency);
             pullPayments[_client][msg.sender].initialPaymentAmountInCents = 0;
         } else {
-            amountInPMA = calculatePMAFromFiat(pullPayments[_client][msg.sender].fiatAmountInCents, pullPayments[_client][msg.sender].currency);   
-            
+            amountInPMA = calculatePMAFromFiat(pullPayments[_client][msg.sender].fiatAmountInCents, pullPayments[_client][msg.sender].currency);
+
             pullPayments[_client][msg.sender].nextPaymentTimestamp = pullPayments[_client][msg.sender].nextPaymentTimestamp + pullPayments[_client][msg.sender].frequency;
             pullPayments[_client][msg.sender].numberOfPayments = pullPayments[_client][msg.sender].numberOfPayments - 1;
         }
@@ -749,7 +750,7 @@ contract PumaPayPullPayment is Ownable {
         emit LogPullPaymentExecuted(_client, msg.sender, pullPayments[_client][msg.sender].paymentID);
     }
 
-    function getRate(string _currency) public view returns(uint256) {
+    function getRate(string _currency) public view returns (uint256) {
         return exchangeRates[_currency];
     }
 
@@ -774,7 +775,7 @@ contract PumaPayPullPayment is Ownable {
     internal
     view
     returns (uint256) {
-        return ((ONE_ETHER * DECIMAL_FIXER * _fiatAmountInCents) / exchangeRates[_currency]) / FIAT_TO_CENT_FIXER;
+        return ONE_ETHER.mul(DECIMAL_FIXER).mul(_fiatAmountInCents).div(exchangeRates[_currency]).div(FIAT_TO_CENT_FIXER);
     }
 
     /// @dev Checks if a registration request is valid by comparing the v, r, s params
@@ -796,7 +797,7 @@ contract PumaPayPullPayment is Ownable {
     )
     internal
     pure
-    returns(bool)
+    returns (bool)
     {
         return ecrecover(
             keccak256(
@@ -809,8 +810,8 @@ contract PumaPayPullPayment is Ownable {
                     _pullPayment.numberOfPayments,
                     _pullPayment.startTimestamp
                 )
-        ),
-        v, r, s) == _client;
+            ),
+            v, r, s) == _client;
     }
 
     /// @dev Checks if a deletion request is valid by comparing the v, r, s params
@@ -832,7 +833,7 @@ contract PumaPayPullPayment is Ownable {
     )
     internal
     view
-    returns(bool)
+    returns (bool)
     {
         return ecrecover(
             keccak256(
@@ -841,9 +842,9 @@ contract PumaPayPullPayment is Ownable {
                     _beneficiary
                 )
             ), v, r, s) == _client
-            && keccak256(
-                abi.encodePacked(pullPayments[_client][_beneficiary].paymentID)
-                ) == keccak256(abi.encodePacked(_paymentID));
+        && keccak256(
+            abi.encodePacked(pullPayments[_client][_beneficiary].paymentID)
+        ) == keccak256(abi.encodePacked(_paymentID));
     }
 
     /// @dev Checks if a payment for a beneficiary of a client exists.
@@ -853,25 +854,25 @@ contract PumaPayPullPayment is Ownable {
     function doesPaymentExist(address _client, address _beneficiary)
     internal
     view
-    returns(bool) {
+    returns (bool) {
         return (
-            bytes(pullPayments[_client][_beneficiary].currency).length > 0 &&
-            pullPayments[_client][_beneficiary].fiatAmountInCents > 0 &&
-            pullPayments[_client][_beneficiary].frequency > 0 &&
-            pullPayments[_client][_beneficiary].startTimestamp > 0 &&
-            pullPayments[_client][_beneficiary].numberOfPayments > 0 &&
-            pullPayments[_client][_beneficiary].nextPaymentTimestamp > 0
+        bytes(pullPayments[_client][_beneficiary].currency).length > 0 &&
+        pullPayments[_client][_beneficiary].fiatAmountInCents > 0 &&
+        pullPayments[_client][_beneficiary].frequency > 0 &&
+        pullPayments[_client][_beneficiary].startTimestamp > 0 &&
+        pullPayments[_client][_beneficiary].numberOfPayments > 0 &&
+        pullPayments[_client][_beneficiary].nextPaymentTimestamp > 0
         );
     }
-    
+
     /// @dev Checks if the address of an owner/executor needs to be funded. 
     /// The minimum amount the owner/executors should always have is 0.001 ETH 
     /// @param _address - address of owner/executors that the balance is checked against. 
     /// @return bool - whether the address needs more ETH.
-    function isFundingNeeded(address _address) 
+    function isFundingNeeded(address _address)
     private
     view
     returns (bool) {
-        return address(_address).balance <= MINIMUM_AMOUN_OF_ETH_FOR_OPARATORS;
+        return address(_address).balance <= MINIMUM_AMOUNT_OF_ETH_FOR_OPARATORS;
     }
 }
