4f3be693abf5ddb55e76be88abe50ce63bf5939a
artemvorobev <artall64@gmail.com>
2019-03-06 15:44:44 +0200

    Specify ganache as a test network for ./test.sh, define ganache rpc settings in truffle.js

--- truffle.js
+++ truffle.js
@@ -9,13 +9,20 @@ module.exports = {
             network_id: '*', // Match any network id
             gasPrice: 1000000000
         },
+        ganache: {
+            host: '127.0.0.1',
+            port: 8545,
+            network_id: 5777,
+            gas: 6721975,
+            gasPrice: 1,
+        },
         coverage: {
             host: "localhost",
             network_id: "*",
             port: 8555,
             gas: 0xfffffffffff,
             gasPrice: 0x01
-        }
+        },
     },
     mocha: {
         useColors: true,
