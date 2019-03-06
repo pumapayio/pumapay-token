5025a65899199cb93ee2f6438f94aa1cffd70ffc
artemvorobev <artall64@gmail.com>
2019-03-06 15:52:42 +0200

    Fix spelling & auto fix of some line endings

--- contracts/PumaPayVault.sol
+++ contracts/PumaPayVault.sol
@@ -4,7 +4,7 @@ import "./PumaPayToken.sol";
 import "../node_modules/openzeppelin-solidity/contracts/ownership/Ownable.sol";
 import "../node_modules/openzeppelin-solidity/contracts/math/SafeMath.sol";
 
-/// @title PumaPayVault - Contract that will hold PMA locked for a specific period of time with predefined withdawal windows for a certain percentage of tokens.
+/// @title PumaPayVault - Contract that will hold PMA locked for a specific period of time with predefined withdrawal windows for a certain percentage of tokens.
 /// @author Giorgos Kourtellos - <giorgos@pumapay.io>
 contract PumaPayVault is Ownable {
     using SafeMath for uint256;
@@ -30,7 +30,7 @@ contract PumaPayVault is Ownable {
     bool public lockedScheduleConstructed;
     uint256[] public intervals;
     uint256[] public percentages;
-    
+
     PumaPayToken public token;
 
     struct LockScheduleDetails {
@@ -62,7 +62,7 @@ contract PumaPayVault is Ownable {
         require(vaultIsUnlocked());
         _;
     }
-    
+
     modifier isLocked() {
         require(!vaultIsUnlocked());
         _;
@@ -93,14 +93,14 @@ contract PumaPayVault is Ownable {
     ///                                      Constructor
     /// =================================================================================================================
 
-    /// @dev Contract constructor sets owner of the vault, the intervals on which the vault will be unlocked 
+    /// @dev Contract constructor sets owner of the vault, the intervals on which the vault will be unlocked
     /// and the percentages of the vault that will be unlocked for the certain intervals
     /// @param _owner Owner of the Vault.
     /// @param _tokenAddress Token Address.
     /// @param _intervals Intervals in days on which the vault will be unlocked.
     /// @param _percentages Percentage of the tokens held by the vault that are unlocked over the specified intervals.
-    constructor(address _owner, PumaPayToken _tokenAddress, uint256[] _intervals, uint256[] _percentages) 
-    public 
+    constructor(address _owner, PumaPayToken _tokenAddress, uint256[] _intervals, uint256[] _percentages)
+    public
     validRequirements(_owner, _tokenAddress, _intervals, _percentages) {
         lockedScheduleConstructed = false;
         token = _tokenAddress;
@@ -118,11 +118,11 @@ contract PumaPayVault is Ownable {
     /// The tokens are transfered to the owner account
     /// @param _amountOfTokens - number of tokens to be withrawn
     function withdrawTokens(uint256 _amountOfTokens)
-        public 
+        public
         onlyOwner()
         lockedScheduleISConstructed()
         isUnlocked()
-        validAmountOfTokens(_amountOfTokens) { 
+        validAmountOfTokens(_amountOfTokens) {
         token.transfer(owner, _amountOfTokens);
         amountOfTokensWithdrawn = amountOfTokensWithdrawn + _amountOfTokens;
         amountOfTokensAllowedForWithdrawal = amountOfTokensAllowedForWithdrawal.sub(_amountOfTokens);
@@ -133,7 +133,7 @@ contract PumaPayVault is Ownable {
 
     /// @dev Sets the next withrawal details i.e. unclocked timestamp and amount of tokens can be executed only by the owner of the vault
     /// and only after the lock schedule has been constructed and the vault is not locked
-    function setNextWithdrawalDetails() 
+    function setNextWithdrawalDetails()
     public
     onlyOwner()
     isLocked()
@@ -153,10 +153,10 @@ contract PumaPayVault is Ownable {
 
     /// @dev Constructs the LockDownSchedule mapping only once and only if the vault holds tokens
     /// can be executed only by the owner of the vault
-    function constructLockedDownSchedule() 
-    public 
+    function constructLockedDownSchedule()
+    public
     onlyOwner()
-    hasTokens() 
+    hasTokens()
     lockedScheduleNOTConstructed() {
         for (uint i = 0; i < intervals.length; i++) {
             lockSchedule.push(
@@ -189,9 +189,9 @@ contract PumaPayVault is Ownable {
     /// Checks if the intervals specified in contract creation are valid
     /// Valid means that each interval should be higher than the previous one and none should be zero
     /// @param _intervals - array of intervals initiating the contract
-    function validIntervals(uint256[] _intervals) 
-    internal 
-    pure 
+    function validIntervals(uint256[] _intervals)
+    internal
+    pure
     returns(bool) {
         uint256 previousInterval = 0;
         for (uint i = 0; i < _intervals.length; i++) {
@@ -207,8 +207,8 @@ contract PumaPayVault is Ownable {
     /// Valid means that each percentages should be higher than the previous one and none should be zero
     /// Also the last value of the array should be 100, otherwise there will be locked tokens with no way of retrieving them
     /// @param _percentages - array of percentages initiating the contract
-    function validPercentages(uint256[] _percentages) 
-    internal 
+    function validPercentages(uint256[] _percentages)
+    internal
     pure
     lastValueOfArrayIs100(_percentages)
     returns(bool) {
